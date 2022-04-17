2afc9c342bda4a8641a007ee7a2478be80b3802d
HritikNegi <91362469+HritikNegi@users.noreply.github.com>
2022-04-17 19:43:11 +0530

    Update P7_SwitchStatement.java

--- Unit_01/P7/P7_SwitchStatement.java
+++ Unit_01/P7/P7_SwitchStatement.java
@@ -22,8 +22,38 @@ public class P7_SwitchStatement {
 
 class SwitchStatements {
 	void switchStatements() {
-		
-		// Write Logic here!
-		
+
+		Scanner obj1 = new Scanner(System.in);
+		System.out.println("Enter your choice");
+
+		int Choice = obj1.nextInt();
+
+		switch (Choice) {
+		case 1: {
+			if (Choice == 1) {
+				System.out.println("2022");
+			}
+			break;
+		}
+
+		case 2: {
+			if (Choice == 2) {
+				System.out.println("April");
+			}
+			break;
+		}
+		case 3: {
+			if (Choice == 3) {
+				System.out.println("Saturday");
+			}
+			break;
+		}
+		default: {
+			System.out.println("Invalid Choice");
+		}
+			break;
+
+		}
+
 	}
 }
